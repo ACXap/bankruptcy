5d40c12cc9d969674f672b42ad9eff7051b14661
Aleksandr <mitkov1@inbox.ru>
2021-03-15 16:56:44 +0700

    init

--- src/main/java/com/rt/bankruptcy/repository/BankruptcyClient.java
+++ src/main/java/com/rt/bankruptcy/repository/BankruptcyClient.java
@@ -2,8 +2,11 @@ package com.rt.bankruptcy.repository;
 
 import com.rt.bankruptcy.repository.soap.GetDebtorByIdBankrupt;
 import com.rt.bankruptcy.repository.soap.GetDebtorByIdBankruptResponse;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 import org.springframework.stereotype.Service;
 import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
+import org.springframework.ws.transport.http.HttpComponentsMessageSender;
 
 @Service
 public class BankruptcyClient extends WebServiceGatewaySupport {
@@ -13,7 +16,11 @@ public class BankruptcyClient extends WebServiceGatewaySupport {
         GetDebtorByIdBankrupt request = new GetDebtorByIdBankrupt();
         request.setIdBankrupt(138895);
 
-        Object obj = soapConnector.callWebService("https://services.fedresurs.ru/Bankruptcy/MessageServiceDemo/WebService.svc", request);
-        System.out.println(obj);
+        try {
+            Object obj = soapConnector.callWebService("https://services.fedresurs.ru/Bankruptcy/MessageServiceDemo/", request);
+            System.out.println(obj);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 }
--- src/main/java/com/rt/bankruptcy/repository/ConfigClient.java
+++ src/main/java/com/rt/bankruptcy/repository/ConfigClient.java
@@ -4,10 +4,36 @@ import org.apache.http.auth.UsernamePasswordCredentials;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+import org.springframework.ws.client.core.WebServiceTemplate;
 import org.springframework.ws.transport.http.HttpComponentsMessageSender;
 
 @Configuration
 public class ConfigClient {
+
+//    @Bean
+//    public ESignatureProcessorClient eSignatureProcessorClient() {
+//        ESignatureProcessorClient client = new ESignatureProcessorClient();
+//        client.setWebServiceTemplate(mwWebServiceTemplate());
+//        return client;
+//    }
+//
+//    @Bean
+//    public WebServiceTemplate mwWebServiceTemplate() {
+//        Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
+//        marshaller.setContextPath("com.rt.bankruptcy.repository.soap");
+//        WebServiceTemplate template = new WebServiceTemplate(marshaller, marshaller);
+//        template.setDefaultUri("https://osb-st2.vs.csin.cz:5001/CSMW/WS_MW_ESignatureProcessor_v02_02");
+//        template.setMessageSender(defaultMwMessageSender());
+//        return template;
+//    }
+//
+//    @Bean
+//    public HttpComponentsMessageSender defaultMwMessageSender() {
+//        HttpComponentsMessageSender messageSender = new HttpComponentsMessageSender();
+//        messageSender.setCredentials(new UsernamePasswordCredentials("user", "password"));
+//        return messageSender;
+//    }
+
     @Bean
     public Jaxb2Marshaller marshaller() {
         Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
@@ -18,7 +44,7 @@ public class ConfigClient {
     @Bean
     public SoapConnector soapConnector(Jaxb2Marshaller marshaller) {
         SoapConnector client = new SoapConnector();
-        client.setDefaultUri("https://services.fedresurs.ru/Bankruptcy/MessageServiceDemo/WebService.svc");
+        client.setDefaultUri("https://services.fedresurs.ru/Bankruptcy/MessageServiceDemo/");
         client.setMarshaller(marshaller);
         client.setUnmarshaller(marshaller);
         client.setMessageSender(httpComponentsMessageSender());
